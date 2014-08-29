03037837da995be805cbbe471bfb86ec5ed0226e
Doucet, Mathieu <doucetm@ornl.gov>
2014-08-29 09:39:22 -0400

    Make sure SNS software is always loaded on Fermi

--- scripts/remoteJob.sh
+++ scripts/remoteJob.sh
@@ -5,6 +5,7 @@
 #PBS -W umask=022
 
 module load mantid-mpi
+module load sns_software
 export OMP_NUM_THREADS=16
 
 mpirun -n $n_nodes python $reduce_script $data_file $proposal_shared_dir
--- scripts/startJob.sh
+++ scripts/startJob.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Usage: startJob.sh <path to processing script> <destination> <data> 
 module load mantid-mpi
+module load sns_software
 python $1 -q $2 -d $3
