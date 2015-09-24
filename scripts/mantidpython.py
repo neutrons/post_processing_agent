bbd2f1a741097f3e3c1b1a3c5a917ed4ebcaae79
Doucet, Mathieu <doucetm@ornl.gov>
2015-09-24 08:40:17 -0400

    update mantid wrapper

--- scripts/mantidpython.py
+++ scripts/mantidpython.py
@@ -1,15 +1,34 @@
 #!/bin/env python
 import os
+import re
 import subprocess
 import sys
+
+mantidRegExp=re.compile(r'/opt/.antid.*/bin')
+def getMantidLoc(line):
+    line = line.strip()
+    if not line.startswith("sys.path"):
+        return None
+    mantidversion = mantidRegExp.findall(line)
+    if len(mantidversion)==1:
+        return mantidversion[0]
+    else:
+        return None
+
+# testing for the extraction function
+"""
+print getMantidLoc("from mantid.simpleapi import ") # should be None
+print getMantidLoc('sys.path.append(os.path.join("/opt/Mantid/bin"))') # HYS
+print getMantidLoc("sys.path.insert(0,'/opt/Mantid/bin')") # REFL
+print getMantidLoc('sys.path.append("/opt/mantidnightly/bin")') # NOM
+"""
 
 reductionScript=file(sys.argv[1], 'r')
 
 mantidpath=None
 for line in reductionScript:
-    if line.startswith(r'sys.path.append("/opt/'):
-        mantidpath=line.replace(r'sys.path.append(','') \
-                    .replace(r')','')[1:-2].strip()
+    mantidpath=getMantidLoc(line)
+    if mantidpath is not None:
         break
 if mantidpath is None:
     raise RuntimeError("Failed to determine mantid version from script: %s" \
@@ -19,6 +38,7 @@ mantidpython=os.path.join(mantidpath, "mantidpython")
 if not os.path.exists(mantidpython):
     raise RuntimeError("Failed to find launcher: '%s'" % mantidpython)
 
+print mantidpython
 cmd=sys.argv[1:]
 cmd.insert(0,mantidpython)
 subprocess.call(cmd)
