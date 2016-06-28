441d57d63c9f9cc765424f91f1706a8aa0a5dbf5
Doucet, Mathieu <doucetm@ornl.gov>
2016-06-28 16:05:29 -0400

    Tweak config file

--- postprocessing/publish_plot.py
+++ postprocessing/publish_plot.py
@@ -2,7 +2,7 @@
     Utility functions to post plot data
 """
 from __future__ import print_function
-from postprocessing.Configuration import Configuration
+from postprocessing.Configuration import Configuration, CONFIG_FILE
 import requests
 import string
 
@@ -10,6 +10,8 @@ def get_user(config_file=None):
     """
         Return username and password from config file
     """
+    if config_file is None:
+        config_file = CONFIG_FILE
     config = Configuration(config_file)
     return {'username': config.publisher_username,
             'password': config.publisher_password}
@@ -18,6 +20,8 @@ def publish_plot(instrument, run_number, files, config_file=None):
     """
         Files should be a dict of file contents to publish e.g. files={'file': div}
     """
+    if config_file is None:
+        config_file = CONFIG_FILE
     config = Configuration(config_file)
 
     url_template = string.Template(config.publish_url)
