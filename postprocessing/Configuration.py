7fb99febc32ece26a0c35ddc667fbc4ea5afa6fd
Jose Borreguero <borreguero@gmail.com>
2021-09-08 13:56:33 -0400

    unit tests for postprocessing.Configuration
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- postprocessing/Configuration.py
+++ postprocessing/Configuration.py
@@ -1,4 +1,4 @@
-#pylint: disable=line-too-long, too-many-statements, too-few-public-methods, too-many-instance-attributes, invalid-name
+# pylint: disable=line-too-long, too-many-statements, too-few-public-methods, too-many-instance-attributes, invalid-name
 """
     Post-processing, ActiveMQ, and logging configuration
 
@@ -11,31 +11,15 @@ import os
 import json
 import logging
 
-class StreamToLogger(object):
-    """
-        File-like stream object that redirects writes to a logger instance.
-    """
-    def __init__(self, logger, log_level=logging.INFO):
-        self.logger = logger
-        self.log_level = log_level
-        self.linebuf = ''
-
-    def write(self, buf):
-        """
-            Write a message to stdout so we can see it when running interactively
-        """
-        sys.stdout.write(buf)
-        for line in buf.rstrip().splitlines():
-            self.logger.log(self.log_level, line.rstrip())
-
 
 class Configuration(object):
     """
         Read and process configuration file and provide an easy way to create a configured Client object
     """
+
     def __init__(self, config_file):
-        if os.access(config_file, os.R_OK) == False:
-            raise RuntimeError, "Configuration file doesn't exist or is not readable: %s" % config_file
+        if os.access(config_file, os.R_OK) is False:
+            raise RuntimeError("Configuration file doesn't exist or is not readable: %s" % config_file)
         cfg = open(config_file, 'r')
         json_encoded = cfg.read()
         config = json.loads(json_encoded)
@@ -51,32 +35,41 @@ class Configuration(object):
         self.sw_dir = config['sw_dir'] if 'sw_dir' in config else '/opt/postprocessing'
         self.postprocess_error = config['postprocess_error']
         # Catalog AMQ queues
-        self.catalog_data_ready = config['catalog_data_ready'] if 'catalog_data_ready' in config else 'CATALOG.DATA_READY'
+        self.catalog_data_ready = config[
+            'catalog_data_ready'] if 'catalog_data_ready' in config else 'CATALOG.DATA_READY'
         self.catalog_started = config['catalog_started']
         self.catalog_complete = config['catalog_complete']
         self.catalog_error = config['catalog_error']
         # Reduction AMQ queues
-        self.reduction_data_ready = config['reduction_data_ready'] if 'reduction_data_ready' in config else 'REDUCTION.DATA_READY'
+        self.reduction_data_ready = config[
+            'reduction_data_ready'] if 'reduction_data_ready' in config else 'REDUCTION.DATA_READY'
         self.reduction_started = config['reduction_started']
         self.reduction_complete = config['reduction_complete']
         self.reduction_error = config['reduction_error']
         self.reduction_disabled = config['reduction_disabled']
-        self.heartbeat_ping = config['heartbeat_ping'] if 'heartbeat_ping' in config else '/topic/SNS.COMMON.STATUS.PING'
+        self.heartbeat_ping = config[
+            'heartbeat_ping'] if 'heartbeat_ping' in config else '/topic/SNS.COMMON.STATUS.PING'
         # Reduction catalog AMQ queues
-        self.reduction_catalog_data_ready = config['reduction_catalog_data_ready'] if 'reduction_catalog_data_ready' in config else 'REDUCTION_CATALOG.DATA_READY'
+        self.reduction_catalog_data_ready = config[
+            'reduction_catalog_data_ready'] if 'reduction_catalog_data_ready' in config else 'REDUCTION_CATALOG.DATA_READY'
         self.reduction_catalog_started = config['reduction_catalog_started']
         self.reduction_catalog_complete = config['reduction_catalog_complete']
         self.reduction_catalog_error = config['reduction_catalog_error']
         # Reduction script writer
-        self.create_reduction_script = config['create_reduction_script'] if 'create_reduction_script' in config else 'REDUCTION.CREATE_SCRIPT'
-        self.service_status = config['service_status'] if 'service_status' in config else "/topic/SNS.${instrument}.STATUS.POSTPROCESS"
+        self.create_reduction_script = config[
+            'create_reduction_script'] if 'create_reduction_script' in config else 'REDUCTION.CREATE_SCRIPT'
+        self.service_status = config[
+            'service_status'] if 'service_status' in config else "/topic/SNS.${instrument}.STATUS.POSTPROCESS"
 
         self.heart_beat = config['heart_beat']
         self.log_file = config['log_file'] if 'log_file' in config else 'post_processing.log'
         self.start_script = config['start_script'] if 'start_script' in config else 'startJob.sh'
         self.task_script = config['task_script'] if 'task_script' in config else 'PostProcessAdmin.py'
-        self.python_dir = config['python_dir'] if 'python_dir' in config else os.path.join(self.sw_dir, 'postprocessing')
-        self.remote_script = config['remote_script'] if 'remote_script' in config else os.path.join(self.sw_dir, 'scripts', 'remoteJob.sh')
+        self.python_dir = config['python_dir'] if 'python_dir' in config else os.path.join(self.sw_dir,
+                                                                                           'postprocessing')
+        self.remote_script = config['remote_script'] if 'remote_script' in config else os.path.join(self.sw_dir,
+                                                                                                    'scripts',
+                                                                                                    'remoteJob.sh')
         self.mantid_path = config['mantid_path'] if 'mantid_path' in config else '/opt/Mantid/bin'
         self.dev_output_dir = config['dev_output_dir'] if 'dev_output_dir' in config else ''
         self.python_executable = config['python_exec'] if 'python_exec' in config else 'python'
@@ -85,12 +78,15 @@ class Configuration(object):
         self.max_memory = config['max_memory'] if 'max_memory' in config else 8.0
         self.max_procs = config['max_procs'] if 'max_procs' in config else 5
         self.processors_per_node = config['processors_per_node'] if 'processors_per_node' in config else 16
-        self.wait_notification_period = config['wait_notification_period'] if 'wait_notification_period' in config else 900
+        self.wait_notification_period = config[
+            'wait_notification_period'] if 'wait_notification_period' in config else 900
 
-        self.web_monitor_url = config['webmon_url_template'] if 'webmon_url_template' in config else "https://monitor.sns.gov/files/$instrument/$run_number/submit_reduced/"
+        self.web_monitor_url = config[
+            'webmon_url_template'] if 'webmon_url_template' in config\
+            else "https://monitor.sns.gov/files/$instrument/$run_number/submit_reduced/"
         self.max_image_size = config['max_image_size'] if 'max_image_size' in config else 500000
-        self.comm_only = config['communication_only']==1 if 'communication_only' in config else False
-        self.remote_execution = config['remote_execution']==1 if 'remote_execution' in config else False
+        self.comm_only = config['communication_only'] == 1 if 'communication_only' in config else False
+        self.remote_execution = config['remote_execution'] == 1 if 'remote_execution' in config else False
 
         self.task_script_queue_arg = config['task_script_queue_arg'] if 'task_script_queue_arg' in config else None
         self.task_script_data_arg = config['task_script_data_arg'] if 'task_script_data_arg' in config else None
@@ -111,72 +107,122 @@ class Configuration(object):
             for p in self.processors:
                 toks = p.split('.')
                 if len(toks) == 2:
-                    processor_module = __import__("postprocessing.processors.%s" % toks[0], globals(), locals(), [toks[1],], -1)
+                    # for instance, emulate `from oncat_processor import ONCatProcessor`
+                    # TODO replace with importlib.import_module()
+                    processor_module = __import__("postprocessing.processors.%s" % toks[0], globals(), locals(),
+                                                  [toks[1], ], -1)
                     try:
                         processor_class = eval("processor_module.%s" % toks[1])
                         self.queues.append(processor_class.get_input_queue_name())
                     except:
                         logging.error("Configuration: Error loading processor: %s", sys.exc_value)
                 else:
-                    logging.error("Configuration: Processors can only be specified in the format module.Processor_class")
+                    logging.error(
+                        "Configuration: Processors can only be specified in the format module.Processor_class")
 
-    def log_configuration(self):
+    def log_configuration(self, logger=logging):
         """
             Log the current configuration
         """
-        logging.info("Using %s", self.config_file)
+        logger.info("Using %s", self.config_file)
         if self.comm_only:
-            logging.info("  - Running in COMMUNICATION ONLY mode: no post-processing will be performed")
+            logger.info("  - Running in COMMUNICATION ONLY mode: no post-processing will be performed")
         if self.remote_execution:
-            logging.info("  - REMOTE execution")
-            logging.info("    Max chunk memory: %s", self.max_memory)
-            logging.info("    Max nodes: %s", self.max_nodes)
+            logger.info("  - REMOTE execution")
+            logger.info("    Max chunk memory: %s", self.max_memory)
+            logger.info("    Max nodes: %s", self.max_nodes)
         else:
-            logging.info("  - LOCAL execution")
-        logging.info("  - Max number of processes: %s", self.max_procs)
-        logging.info("  - Input queues: %s", self.queues)
-        logging.info("  - Installation dir: %s", self.sw_dir)
-        logging.info("  - Start script: %s", self.start_script)
-        logging.info("  - Task script: %s", self.task_script)
-        logging.info("  - Image posting URL: %s", self.web_monitor_url)
-        logging.info("  - Error exceptions: %s", str(self.exceptions))
+            logger.info("  - LOCAL execution")
+        logger.info("  - Max number of processes: %s", self.max_procs)
+        logger.info("  - Input queues: %s", self.queues)
+        logger.info("  - Installation dir: %s", self.sw_dir)
+        logger.info("  - Start script: %s", self.start_script)
+        logger.info("  - Task script: %s", self.task_script)
+        logger.info("  - Image posting URL: %s", self.web_monitor_url)
+        logger.info("  - Error exceptions: %s", str(self.exceptions))
+
 
 # Set the log level for the Stomp client
 stomp_logger = logging.getLogger('stompest.sync.client')
 stomp_logger.setLevel(logging.ERROR)
 
+
+class StreamToLogger(object):
+    r"""File-like stream object that redirects writes to a Logger instance."""
+
+    def __init__(self, logger, log_level=logging.INFO):
+        r"""
+        @brief duck-typing for a file-stream object that redirects to a Logger instance
+        @param Logger logger: instance of python standard Logger
+        @param int log_level: one of DEBUG, INFO, WARNING, ERROR, CRITICAL
+        """
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
+
+    def write(self, buf):
+        """
+            Write a message to stdout so we can see it when running interactively
+        """
+        sys.stdout.write(buf)
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+
+def initialize_logging(log_file, level=logging.INFO, preemptive_cleanup=False):
+    r"""
+    @brief Set the default log level and the file where to append log messages
+
+    @details: also pipe sys.stderr to the STDERR channel of the root logger
+
+    @param str log_file: absolute path to the file logging the messages
+    @param int level: one of DEBUG, INFO, WARNING, ERROR, CRITICAL
+    @param bool preemptive_cleanup: remove all existing handlers of the root looger before initializing
+    """
+    # logging.basicConfig does nothing if the root logger already has handlers
+    if preemptive_cleanup:
+        for handler in logging.root.handlers:
+            handler.close()
+            logging.root.removeHandler(handler)
+
+    logging.basicConfig(
+        level=level,
+        format="%(asctime)s %(levelname)s/%(process)d %(message)s",
+        filename=log_file,
+        filemode='a'
+    )
+    stderr_logger = logging.getLogger('STDERR')
+    sl = StreamToLogger(stderr_logger, logging.ERROR)
+    sys.stderr = sl
+
+
 # Default locations for configurationss
 CONFIG_FILE = '/etc/autoreduce/post_processing.conf'
 CONFIG_FILE_ALTERNATE = '/sw/fermi/autoreduce/postprocessing/configuration/post_processing.conf'
 
-def read_configuration(config_file=None):
+
+def read_configuration(config_file=None, defaults=[CONFIG_FILE, CONFIG_FILE_ALTERNATE]):
+    r"""
+    Returns a new configuration object for a given configuration file, and initializes the basic configuration
+    of all log messages.
+
+    @details: also initialize the logging (see `initialize_logging`)
+
+    @param str config_file: absolute path to custom configuration file to process
+    @param list defaults: configuration files to be used when no custom configuration is provided
+
+    @returns Configuration: data structure representing the configuration file just read
     """
-        Returns a new configuration object for a given
-        configuration file
-        @param config_file: configuration file to process
-    """
+
     if config_file is None:
-        # Make sure we have a configuration file to read
-        config_file = CONFIG_FILE
-        if os.access(config_file, os.R_OK) == False:
-            config_file = CONFIG_FILE_ALTERNATE
-            if os.access(config_file, os.R_OK) == False:
-                raise RuntimeError("Configuration file doesn't exist or is not readable: %s" % CONFIG_FILE)
+        for config_default in defaults:
+            if os.access(config_default, os.R_OK):
+                config_file = config_default
+                break
+        else:
+            raise RuntimeError("Default configuration file(s) do not exist, or unreadable: %s" % str(defaults))
 
     configuration = Configuration(config_file)
-    logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s %(levelname)s/%(process)d %(message)s",
-        filename=configuration.log_file,
-        filemode='a'
-    )
-
-    #stdout_logger = logging.getLogger('STDOUT')
-    #sl = StreamToLogger(stdout_logger, logging.INFO)
-    #sys.stdout = sl
-
-    stderr_logger = logging.getLogger('STDERR')
-    sl = StreamToLogger(stderr_logger, logging.ERROR)
-    sys.stderr = sl
+    initialize_logging(configuration.log_file)
 
     return configuration
--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,11 @@
 import pytest
 
 # standard imports
+from collections import namedtuple
+import logging
 import os
 import sys
+import tempfile
 
 this_module_path = sys.modules[__name__].__file__
 
@@ -29,3 +32,25 @@ def data_server():
             return file_path
 
     return _DataServe()
+
+
+@pytest.yield_fixture(scope='function')  # 'yield_fixture' deprecated in favor of 'yield' when using python 3.x
+def test_logger():
+    r"""
+    Instantiate a Logger that writes messages to a temporary file
+    @returns namedtuple('TestLogger', ['logger', 'log_file']) where 'logger' is a Logger object
+    and 'log_file' is the absolute path to the file recording the log messages
+    """
+    logger = logging.getLogger('test')
+    logger.setLevel(logging.DEBUG)
+    _, log_file = tempfile.mkstemp()
+    fh = logging.FileHandler(log_file)
+    fh.setLevel(logging.DEBUG)
+    logger.addHandler(fh)
+    TestLogger = namedtuple('TestLogger', ['logger', 'log_file'])
+    yield TestLogger(logger, log_file)
+    # clean up time
+    for handler in logger.handlers:
+        handler.close()
+        logger.removeHandler(handler)
+    os.remove(log_file)
