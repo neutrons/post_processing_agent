7db12c3464d8e32a0d4f1b0eccced3182f9a8872
Ross Whitfield <whitfieldre1@ornl.gov>
2023-12-01 13:56:11 +1100

    Add test for REDUCTION_CATALOG

--- tests/integration/test_cataloging.py
+++ tests/integration/test_cataloging.py
@@ -7,7 +7,7 @@ from stompest.error import StompConnectTimeout
 
 
 def test_oncat_catalog():
-    """This should just replace reduce_TOPAZ.py with reduce_TOPAZ_default.py"""
+    """This should run ONCatProcessor"""
     message = {
         "run_number": "30892",
         "instrument": "EQSANS",
@@ -27,6 +27,41 @@ def test_oncat_catalog():
 
     # expect a message on CATALOG.ONCAT.COMPLETE
     client.subscribe("/queue/CATALOG.ONCAT.COMPLETE")
+
+    assert client.canRead(5)
+    frame = client.receiveFrame()
+
+    client.disconnect()
+
+    msg = json.loads(frame.body)
+    assert msg["run_number"] == message["run_number"]
+    assert msg["instrument"] == message["instrument"]
+    assert msg["ipts"] == message["ipts"]
+    assert msg["facility"] == message["facility"]
+    assert msg["data_file"] == message["data_file"]
+
+
+def test_oncat_reduction_catalog():
+    """This should run reduction ONCatProcessor"""
+    message = {
+        "run_number": "30892",
+        "instrument": "EQSANS",
+        "ipts": "IPTS-10674",
+        "facility": "SNS",
+        "data_file": "/SNS/EQSANS/IPTS-10674/0/30892/NeXus/EQSANS_30892_event.nxs",
+    }
+
+    client = Stomp(StompConfig("tcp://localhost:61613"))
+    try:
+        client.connect()
+    except StompConnectTimeout:
+        pytest.skip("Requires activemq running")
+
+    # send data ready
+    client.send("/queue/REDUCTION_CATALOG.DATA_READY", json.dumps(message))
+
+    # expect a message on REDUCTION_CATALOG.COMPLETE
+    client.subscribe("/queue/REDUCTION_CATALOG.COMPLETE")
 
     assert client.canRead(5)
     frame = client.receiveFrame()
