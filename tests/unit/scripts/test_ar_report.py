60156146803f48ce044ac8d092b33e333f4c706e
Pete Peterson <petersonpf@ornl.gov>
2024-01-05 11:04:30 -0500

    Add unit test for GenericFile

--- tests/unit/scripts/test_ar_report.py
+++ tests/unit/scripts/test_ar_report.py
@@ -1,4 +1,8 @@
+from ar_report import GenericFile
+
+import datetime
 import h5py
+import os
 import pytest
 import shutil
 import tempfile
@@ -42,6 +46,50 @@ def test_main_append():
 
 def test_main_argError():
     pass
+
+
+def test_generic_file():
+    CONTENTS = "hello!"
+    NOW = datetime.datetime.now()
+
+    # create a file with non-empty contents
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as handle:
+        handle.write(CONTENTS)
+        handle.close()
+
+        try:
+            genericfile = GenericFile(handle.name)
+            assert genericfile  # anything goes wrong and this is false
+            assert len(genericfile.filename) > 0  # there was a file created
+            assert genericfile.filename == handle.name
+
+            # length is compatible with contents written
+            assert genericfile.filesize == len(CONTENTS)
+            pytest.approx(
+                genericfile.filesizeMiB(), float(len(CONTENTS)) / 1024.0 / 1024.0
+            )
+            assert genericfile.filesizehuman() == f"{len(CONTENTS)}B"  # bytes
+
+            # creation time is within 0.1s of when the test was started
+            delta = genericfile.timeCreation - NOW
+            assert abs(delta.total_seconds()) < 0.1
+            # only up through the minutes are in GenericFile's representation
+            assert genericfile.iso8601() == genericfile.timeCreation.isoformat()[:16]
+        finally:
+            # remove the temporary file
+            os.unlink(handle.name)
+
+
+def test_generic_file_empty():
+    # create a file with empty contents
+    with tempfile.NamedTemporaryFile(mode="w", delete=False) as handle:
+        handle.close()
+
+        try:
+            assert not GenericFile(handle.name)
+        finally:
+            # remove the temporary file
+            os.unlink(handle.name)
 
 
 logfile_path = "tests/unit/scripts/PG3_56301.nxs.log"
